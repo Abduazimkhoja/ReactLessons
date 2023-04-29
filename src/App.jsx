8e0bbec3f44e495494f987e545103091339d4a35
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-04-29 19:07:33 +0500

    State: input, select, checkbox

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Student from './state';
+import "./App.css";
+
+class App extends React.Component {
+   render() {
+      return (
+         <div>
+            <Student/>
+         </div>
+      )
+   }
+}
+
+export default App;
