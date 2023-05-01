33819324d127bcc3591f14c67ce74f0bb27a8349
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-01 22:32:11 +0500

    State CRUD

---
+++ src/mock.js
@@ -0,0 +1,62 @@
+export const people = [
+   {
+     id: 1,
+     name: 'John Smith',
+     age: 32,
+     address: '123 Main St',
+     status: 'single',
+     nickname: 'JS',
+     univ: 'Harvard',
+     job: 'Software Engineer'
+   },
+   {
+     id: 2,
+     name: 'Mary Walker',
+     age: 25,
+     address: '456 Elm St',
+     status: 'married',
+     nickname: 'MW',
+     univ: 'Stanford',
+     job: 'Doctor'
+   },
+   {
+     id: 3,
+     name: 'David Johnson',
+     age: 40,
+     address: '789 Oak St',
+     status: 'divorced',
+     nickname: 'DJ',
+     univ: 'MIT',
+     job: 'Lawyer'
+   },
+   {
+     id: 4,
+     name: 'Maria Garcia',
+     age: 27,
+     address: '444 Maple Ave',
+     status: 'married',
+     nickname: 'MG',
+     univ: 'NYU',
+     job: 'Marketing Manager'
+   },
+   {
+     id: 5,
+     name: 'Alex Kim',
+     age: 22,
+     address: '888 Broadway',
+     status: 'single',
+     nickname: 'AK',
+     univ: 'Princeton',
+     job: 'Student'
+   },
+   {
+     id: 6,
+     name: 'Sarah Lee',
+     age: 35,
+     address: '321 Pine St',
+     status: 'married',
+     nickname: 'SL',
+     univ: 'Columbia',
+     job: 'Journalist'
+   }
+ ]
--- src/table.jsx
+++ src/table.jsx
@@ -1,137 +1,286 @@
 import React from "react";
 import "./table.scss";
+import { people } from "./mock";
 
 class Student extends React.Component {
    constructor(props) {
       super(props);
-      this.state = [
-         {
-            id: 1,
-            name: "none",
-            age: 0,
-            address: "none",
-            status: "student",
-            nickname: "none",
-            univ: "none",
-            job: "none",
-         },
-         {
-            id: 2,
-            name: "none",
-            age: 0,
-            address: "none",
-            status: "student",
-            nickname: "none",
-            univ: "none",
-            job: "none",
-         },
-         {
-            id: 3,
-            name: "none",
-            age: 0,
-            address: "none",
-            status: "student",
-            nickname: "none",
-            univ: "none",
-            job: "none",
-         },
-      ];
+      this.state = {
+         data: people,
+
+         name: "",
+         age: "",
+         address: "",
+         status: "",
+
+         createError: "",
+
+         selectSearch: "name",
+
+         active: null,
+      };
    }
 
    render() {
-      let count = 0
-      const edit = (event) => {
-         if (count === 0) {
-            let classInput = document.querySelectorAll(".table__input")
-            classInput[0].setAttribute("readonly")
+      // SEARCH
+      const search = (event) => {
+         this.setState({
+            data: people.filter((item) =>
+               `${item[this.state.selectSearch]}`.includes(event.target.value)
+            ),
+         });
+      };
+
+      // ADD
+      const create = () => {
+         let { name, age, address, status } = this.state;
+         if (
+            name.length > 0 &&
+            age > 0 &&
+            address.length > 0 &&
+            status.length > 0
+         ) {
+            let user = {
+               id: this.state.data.length + 1,
+               name,
+               age,
+               address,
+               status,
+               nickname: "1",
+               univ: "1",
+               job: "1",
+            };
+            this.setState({
+               data: [...this.state.data, user],
+               name: "",
+               age: "",
+               address: "",
+               status: "",
+               createError: "",
+            });
+         } else {
+            this.setState({
+               createError: "fill in all Input!",
+            });
          }
       };
-      const inputValue = (lastEvent) => {
-         let defaultInput = lastEvent.target.defaultValue;
-         console.log(this.state[0][defaultInput]); // добавляю атрибкт который делает input не доступным
+
+      // SELECT SEARCH
+      const selectSearch = (event) => {
+         this.setState({ selectSearch: event.target.value });
       };
+
+      // DELETE
+      const onDelete = (id) => {
+         this.setState({
+            data: this.state.data.filter((v) => v.id !== id),
+         });
+      };
+
+      // EDIT
+      const onEdit = (data, isSave) => {
+         if (isSave) {
+            let res = this.state.data.map((value) =>
+               value.id == this.state.active.id
+                  ? {
+                       ...value,
+                       name: this.state.name,
+                       age: this.state.age,
+                       address: this.state.address,
+                       status: this.state.status,
+                    }
+                  : value
+            );
+            this.setState({ active: null, data: res });
+         } else {
+            this.setState({
+               name: data.name,
+               age: data.age,
+               address: data.address,
+               status: data.status,
+               active: { ...data },
+            });
+         }
+      };
+
+      // multi state change
+      const onChange = (event) => {
+         this.setState({ [event.target.name]: event.target.value });
+      };
+
       return (
-         <>
+         <div>
+            {/* SEARCH */}
+            <div className="search">
+               {/* SELECT */}
+               <select onChange={selectSearch} name="" id="">
+                  <option value="name">name</option>
+                  <option value="id">id</option>
+                  <option value="age">age</option>
+                  <option value="address">address</option>
+                  <option value="status">status</option>
+               </select>
+               <input
+                  name="search"
+                  onChange={search}
+                  className="search__input"
+                  type="search"
+                  placeholder="Search"
+               />
+            </div>
+
+            {/* ADD */}
+            <div className="add">
+               <input
+                  name="name"
+                  onChange={onChange}
+                  value={this.state.name}
+                  type="text"
+                  className="add__input"
+                  placeholder="name"
+               />
+               <input
+                  name="age"
+                  onChange={onChange}
+                  value={this.state.age}
+                  type="text"
+                  className="add__input"
+                  placeholder="age"
+               />
+               <input
+                  name="address"
+                  onChange={onChange}
+                  value={this.state.address}
+                  type="text"
+                  className="add__input"
+                  placeholder="address"
+               />
+               <input
+                  name="status"
+                  onChange={onChange}
+                  value={this.state.status}
+                  type="text"
+                  className="add__input"
+                  placeholder="status"
+               />
+               <button onClick={create}>Add</button>
+               <p>{this.state.createError}</p>
+            </div>
+
+            {/* TABLE */}
             <table className="table">
                <thead className="table__head">
-                  <th className="table__title">ID</th>
-                  <th className="table__title">NAME</th>
-                  <th className="table__title">AGE</th>
-                  <th className="table__title">ADDRESS</th>
-                  <th className="table__title">STATUS</th>
-                  <th className="table__title">NICKNAME</th>
-                  <th className="table__title">UNIV</th>
-                  <th className="table__title">JOB</th>
-                  <th className="table__title">EDIT</th>
-               </thead>
-               <tbody className="table__body">
                   <tr>
-                     <td className="table__value">{this.state[0].id}</td>
-                     <td className="table__value">
-                        <input
-                           onChange={inputValue}
-                           className="table__input table__input-active"
-                           type="text"
-                           defaultValue={this.state[0].name}
-                        />
-                     </td>
-                     <td className="table__value">
-                        <input
-                           onChange={inputValue}
-                           className="table__input table__input-active"
-                           type="text"
-                           defaultValue={this.state[0].age}
-                        />
-                     </td>
-                     <td className="table__value">
-                        <input
-                           onChange={inputValue}
-                           className="table__input table__input-active"
-                           type="text"
-                           defaultValue={this.state[0].address}
-                        />
-                     </td>
-                     <td className="table__value">
-                        <input
-                           onChange={inputValue}
-                           className="table__input table__input-active"
-                           type="text"
-                           defaultValue={this.state[0].status}
-                        />
-                     </td>
-                     <td className="table__value">
-                        <input
-                           onChange={inputValue}
-                           className="table__input table__input-active"
-                           type="text"
-                           defaultValue={this.state[0].nickname}
-                        />
-                     </td>
-                     <td className="table__value">
-                        <input
-                           onChange={inputValue}
-                           className="table__input table__input-active"
-                           type="text"
-                           defaultValue={this.state[0].univ}
-                        />
-                     </td>
-                     <td className="table__value">
-                        <input
-                           onChange={inputValue}
-                           className="table__input table__input-active"
-                           type="text"
-                           defaultValue={this.state[0].job}
-                        />
-                     </td>
-                     <td>
-                        <button onClick={edit} className="table__edit">
-                           edit
-                        </button>
-                     </td>
+                     <th>ID</th>
+                     <th>NAME</th>
+                     <th>AGE</th>
+                     <th>ADDRESS</th>
+                     <th>STATUS</th>
+                     <th>NICKNAME</th>
+                     <th>UNIV</th>
+                     <th>JOB</th>
+                     <th>EDIT</th>
                   </tr>
+               </thead>
+
+               <tbody className="table__body">
+                  {this.state.data.map(
+                     ({
+                        id,
+                        name,
+                        age,
+                        address,
+                        status,
+                        nickname,
+                        univ,
+                        job,
+                     }) => {
+                        return (
+
+                           // EDIT
+                           <tr>
+                              <td>{id}</td>
+                              <td>
+                                 {this.state.active?.id === id ? (
+                                    <input
+                                       onChange={onChange}
+                                       value={this.state.name}
+                                       type="text"
+                                       name="name"
+                                       id=""
+                                    />
+                                 ) : (
+                                    name
+                                 )}
+                              </td>
+                              <td>
+                                 {this.state.active?.id === id ? (
+                                    <input
+                                       onChange={onChange}
+                                       value={this.state.age}
+                                       type="text"
+                                       name="age"
+                                       id=""
+                                    />
+                                 ) : (
+                                    age
+                                 )}
+                              </td>
+                              <td>
+                                 {this.state.active?.id === id ? (
+                                    <input
+                                       onChange={onChange}
+                                       value={this.state.address}
+                                       type="text"
+                                       name="address"
+                                       id=""
+                                    />
+                                 ) : (
+                                    address
+                                 )}
+                              </td>
+                              <td>
+                                 {this.state.active?.id === id ? (
+                                    <input
+                                       onChange={onChange}
+                                       value={this.state.status}
+                                       type="text"
+                                       name="status"
+                                       id=""
+                                    />
+                                 ) : (
+                                    status
+                                 )}
+                              </td>
+                              <td>{nickname}</td>
+                              <td>{univ}</td>
+                              <td>{job}</td>
+
+                              {/* DELETE */}
+                              <td id="table__button">
+                                 <button
+                                    onClick={() =>
+                                       onEdit(
+                                          { id, name, age, address, status },
+                                          this.state.active?.id === id
+                                       )
+                                    }
+                                 >
+                                    {this.state.active?.id === id
+                                       ? "save"
+                                       : "edit"}
+                                 </button>
+                                 <button onClick={() => onDelete(id)}>
+                                    delete
+                                 </button>
+                              </td>
+                           </tr>
+                        );
+                     }
+                  )}
                </tbody>
             </table>
-         </>
+         </div>
       );
    }
 }
