993c64a56fa4078130f3053eeb593e53a3daebc9
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-11 19:04:03 +0500

    branch create

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.scss";
+import User from "./lesson/props";
+
+
+function App() {
+   return <div className="App">
+      <User/>
+   </div>;
+}
+
+export default App;
---
+++ src/lesson/props.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+export default class User extends React.Component {
+   constructor(props) {
+      super(props)= {
+
+      };
+   }
+
+   render() {
+
+      return (
+         <>
+
+         </>
+      );
+   }
+}
