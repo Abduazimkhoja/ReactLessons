6bd48cf49273fd23f7dd8e830035412a643920b9
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-15 23:06:42 +0500

    react CRUD

--- src/lesson/index.jsx
+++ src/lesson/index.jsx
@@ -6,6 +6,7 @@ export default class User extends React.Component {
    state = {
       data: people,
       select: "id",
+      clicked: null,
    };
 
    render() {
@@ -17,7 +18,9 @@ export default class User extends React.Component {
       // search
       const onChange = ({ target: { value } }) => {
          this.setState({
-            data: people.filter((v) => `${v[this.state.select]}`.includes(value)),
+            data: people.filter((v) =>
+               `${v[this.state.select]}`.includes(value)
+            ),
          });
       };
 
@@ -27,7 +30,34 @@ export default class User extends React.Component {
       };
 
       // sort
-      
+      const onFilter = () => {
+         this.setState({
+            data: this.state.data.sort((a, b) =>
+               `${a[this.state.select]}`.localeCompare(b[this.state.select])
+            ),
+         });
+      };
+
+      // clicked User
+      const clickedUser = (obj) => {
+         this.state.clicked
+            ? this.setState({ clicked: null })
+            : this.setState({ clicked: obj });
+      };
+
+      // edit
+      const onEdit = (event) => {
+         let {target: {value, name}} = event
+         this.setState({clicked: {...this.state.clicked, [name]:value}})
+      }
+
+      // save 
+      const onSaveUser = () => {
+         this.setState({data: 
+            this.state.data.map(v => v.id === this.state.clicked.id ? this.state.clicked: v)
+         })
+         clickedUser()
+      }
 
       return (
          <div className="container">
@@ -43,6 +73,7 @@ export default class User extends React.Component {
                   <option value="job">job</option>
                </select>
                <input onChange={onChange} type="text" placeholder="search" />
+               <button onClick={onFilter}>Sort</button>
             </div>
             <table className="table">
                <thead className="table__head">
@@ -60,7 +91,7 @@ export default class User extends React.Component {
                   </tr>
                </thead>
                <tbody className="table__body">
-                  {this.state.data.map((data) => {
+                  {this.state.data.map((obj) => {
                      let {
                         id,
                         name,
@@ -70,19 +101,73 @@ export default class User extends React.Component {
                         nickname,
                         univ,
                         job,
-                     } = data;
+                     } = obj;
                      return (
                         <tr key={id} className="table__block">
                            <td>{id}</td>
-                           <td>{name}</td>
-                           <td>{age}</td>
-                           <td>{address}</td>
-                           <td>{status}</td>
-                           <td>{nickname}</td>
-                           <td>{univ}</td>
-                           <td>{job}</td>
                            <td>
-                              <button>edit</button>
+                              {" "}
+                              {this.state.clicked?.id === id ? (
+                                 <input name="name" onChange={onEdit} value={this.state.clicked.name} type="text" />
+                              ) : (
+                                 name
+                              )}
+                           </td>
+                           <td>
+                              {this.state.clicked?.id === id ? (
+                                 <input name="age" onChange={onEdit} value={this.state.clicked.age} type="text" />
+                              ) : (
+                                 age
+                              )}
+                           </td>
+                           <td>
+                              {this.state.clicked?.id === id ? (
+                                 <input name="address" onChange={onEdit} value={this.state.clicked.address} type="text" />
+                              ) : (
+                                 address
+                              )}
+                           </td>
+                           <td>
+                              {this.state.clicked?.id === id ? (
+                                 <input name="status" onChange={onEdit} value={this.state.clicked.status} type="text" />
+                              ) : (
+                                 status
+                              )}
+                           </td>
+                           <td>
+                              {this.state.clicked?.id === id ? (
+                                 <input name="nickname" onChange={onEdit} value={this.state.clicked.nickname} type="text" />
+                              ) : (
+                                 nickname
+                              )}
+                           </td>
+                           <td>
+                              {this.state.clicked?.id === id ? (
+                                 <input name="univ" onChange={onEdit} value={this.state.clicked.univ} type="text" />
+                              ) : (
+                                 univ
+                              )}
+                           </td>
+                           <td>
+                              {this.state.clicked?.id === id ? (
+                                 <input name="job" onChange={onEdit} value={this.state.clicked.job} type="text" />
+                              ) : (
+                                 job
+                              )}
+                           </td>
+                           <td>
+                              {this.state.clicked?.id === id ? (
+                                 <>
+                                    <button onClick={onSaveUser}>save</button>
+                                    <button onClick={clickedUser}>
+                                       cancel
+                                    </button>
+                                 </>
+                              ) : (
+                                 <button onClick={() => clickedUser(obj)}>
+                                    edit
+                                 </button>
+                              )}
                            </td>
                            <td>
                               <button onClick={() => onDelete(id)}>
