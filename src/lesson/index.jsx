dd9791c1ea0b09bbd1bac8fdf7bfd5a3c866f759
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-20 11:10:57 +0500

    Live Circle, Binding

--- src/lesson/index.jsx
+++ src/lesson/index.jsx
@@ -6,12 +6,60 @@ export default class Lesson extends React.Component {
       this.state = {
          count: 0,
       };
-      console.log("constructor");
+      this.plus = this.plus.bind(this)
+   }
+   // LIVE CIRCLE
+
+   componentWillMount() {
+      // до render
+      console.log("componentWillMount");
+   }
+
+   componentDidMount() {
+      // после render
+      console.log("componentDidMount");
+   }
+
+   componentWillReceiveProps() {
+      // когда пришол prop
+      console.log("componentWillReceiveProps");
+   }
+
+   shouldComponentUpdate() {
+      // обновлятли component?
+      console.log("shouldComponentUpdate");
+      return true; // обновлять, On reRender
+      return false; // не обновлять, Off reRender
+   }
+
+   componentWillUpdate() {
+      // до render, после prop
+      console.log("componentWillUpdate");
+   }
+
+   componentDidUpdate() {
+      // после перерисовки или рисовки DOM
+      console.log("componentDidUpdate");
+   }
+
+   componentWillUnmount() {
+      // после удаления элемента из DOM
+      console.log("componentWillUnmount");
+   }
+
+   // BINING
+
+   plus() {
+      this.setState({count: this.state.count + 1})
    }
 
    render() {
       console.log("render");
-
-      return <></>;
+      return (
+         <>
+            <h2>{this.state.count}</h2>
+            <button onClick={this.plus}></button>
+         </>
+      );
    }
 }
