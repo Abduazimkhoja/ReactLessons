baee7c0115f9c82cc6dbc80dbb52d70068a22f20
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-20 14:46:01 +0500

    useReducer

--- src/lesson/index.jsx
+++ src/lesson/index.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useReducer } from "react";
 
-export default class Lesson extends React.Component {
-   render () {
-      return (
-         <>
-            
-         </>
-      )
-   }
+export default function Lesson() {
+   // const [counter, dispatch] = useReducer(()=>{}, 0) // dispatch запускает callback function
+
+   // const [counter, dispatch] = useReducer(() => "callback", 0);
+   // return callback присваивается в counter
+
+   const reducer = (state, action) =>
+      ({
+         plus: state + (action.payload || 1),
+         minus: state - (action.payload || 1),
+         state,
+      }[action.type || "state"]);
+
+   const [counter, dispatch] = useReducer(reducer, 0);
+   // у callback есть 2 параметра. первый это само значение, второе это параметр указаный в despatch
+
+   // return callback присваивается в counter
+   return (
+      <div>
+         <h1>{counter}</h1>
+         <button onClick={() => dispatch({ type: "minus" })}>minus</button>
+         <button onClick={() => dispatch({ type: "plus" })}>plus</button>
+         <button onClick={() => dispatch({ type: "plus", payload: 20 })}>
+            pyload 20
+         </button>
+      </div>
+   );
 }
