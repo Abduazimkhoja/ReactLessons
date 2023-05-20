23fc2d6f44a904195f11d28c70b7d8c75c441fee
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-20 12:58:28 +0500

    Function component, useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import "./App.css";
-import Lesson from "./lesson";
+import {Lesson} from "./lesson";
 
 export default class App extends React.Component {
    render() {
--- src/lesson/index.jsx
+++ src/lesson/index.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default class Lesson extends React.Component {
-   render () {
-      return (
-         <>
-            
-         </>
-      )
-   }
-}
+export const Lesson = () => {
+   const [data, setData] = useState({ name: "John", count: 0 }); // DE structuring useState
+
+   return (
+      <div>
+         <div>{data.name}</div>
+         <input
+            value={data.name}
+            type="text"
+            onChange={(event) => setData({ ...data, name: event.target.value })}
+         />
+         <div>{data.count}</div>
+         <button onClick={() => setData({ ...data, count: data.count + 1 })}>
+            {" "}
+            click
+         </button>
+      </div>
+   );
+};
