0dc98d6285eaa7c5bd8d010495d7fa907c1f830d
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-20 14:17:07 +0500

    useEffect: 4 case

--- src/lesson/index.jsx
+++ src/lesson/index.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
-export default class Lesson extends React.Component {
-   render () {
-      return (
-         <>
-            
-         </>
-      )
-   }
+export default function Lesson() {
+
+   const [count, setCount] = useState(0)
+   const [age, setAge] = useState(0)
+
+   // case 1
+   useEffect(() => { // отслеживает каждое изменение в dom 
+      console.log("case-1");
+   })
+
+   // case 2
+   useEffect(() => { // срабатывает 1 раз при запуске componenta 
+      console.log("case-2");
+   }, [])
+   
+   // case 3
+   useEffect(() => { // срабатывает при изменении переменной count
+      console.log("case-3");
+   }, [count])
+
+   // case 4
+   useEffect(() => { // срабатывает при изменении переменной count и age
+      console.log("case-4");
+   }, [count, age])
+
+   return <div>
+      <h1>{count}</h1>
+      <h1>{age}</h1>
+      <button onClick={() => setCount(count + 1)}>Count</button>
+      <button onClick={() => setAge(age + 1)}>Age</button>
+   </div>;
 }
