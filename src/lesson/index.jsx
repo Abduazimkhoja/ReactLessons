2b329faf7eefd58f746b7a248bec5ab63d3759a9
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-21 15:40:30 +0500

    useReducer

--- src/lesson/index.jsx
+++ src/lesson/index.jsx
@@ -1,37 +1,31 @@
 import React, { useEffect, useReducer, useState } from "react";
 
 export default function Lesson() {
-   // const [count, setCount] = useState(0)
    const [counter, dispatch] = useReducer(
-      (state, action) => {
-         return {
-            plus: { ...state, count: state.count + action.payload },
-            minus: { ...state, count: state.count - action.payload },
-         }[action.type];
-      },
-      { count: 0, type: "plus", payload: 1 }
+      (state, action) =>
+         ({
+            plus: { ...state, count: state.count + state.payload },
+            minus: { ...state, count: state.count - state.payload },
+            payload: { ...state, payload: action },
+         }[action.type || "payload"]),
+      {
+         count: 0,
+         payload: 1,
+      }
    );
 
    return (
       <div>
-         <select
-            onChange={({ target }) =>
-               dispatch({ ...counter, payload: Number(target.value) })
-            }
-         >
+         <select onChange={({ target }) => dispatch(Number(target.value))}>
             <option value={1}>1</option>
             <option value={2}>2</option>
             <option value={3}>3</option>
             <option value={4}>4</option>
             <option value={5}>5</option>
          </select>
-         <button onClick={() => dispatch({ ...counter, type: "minus" })}>
-            minus
-         </button>
          {counter.count}
-         <button onClick={() => dispatch({ ...counter, type: "plus" })}>
-            plus
-         </button>
+         <button onClick={() => dispatch({ type: "plus" })}>plus</button>
+         <button onClick={() => dispatch({ type: "minus" })}>minus</button>
       </div>
    );
 }
