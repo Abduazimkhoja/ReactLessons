9ec096f1f7a1ebab578b6d57125e6b3a8b968a1b
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-21 14:44:40 +0500

    useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-
-import Lesson from "./lesson/";
+import React from 'react'
+import Lesson from './lesson';
 
 
 function App() {
-   return <div className="App">
-      <Lesson type={"true"}/>
-   </div>;
+   return (
+      <div className="App">
+         <Lesson/>
+      </div>
+   );
 }
 
 export default App;
--- src/lesson/index.jsx
+++ src/lesson/index.jsx
@@ -1,66 +1,37 @@
-import React from "react";
-import FuncComponent from "../function component";
+import React, { useEffect, useReducer, useState } from "react";
 
-export default class Lesson extends React.Component {
-   constructor(props) {
-      super(props);
-      this.state = {
-         count: 0,
-      };
-      console.log("constructor");
-   }
+export default function Lesson() {
+   // const [count, setCount] = useState(0)
+   const [counter, dispatch] = useReducer(
+      (state, action) => {
+         return {
+            plus: { ...state, count: state.count + action.payload },
+            minus: { ...state, count: state.count - action.payload },
+         }[action.type];
+      },
+      { count: 0, type: "plus", payload: 1 }
+   );
 
-   componentWillMount() {
-      // до render
-      console.log("componentWillMount");
-   }
-
-   componentDidMount() {
-      // после render
-      console.log("componentDidMount");
-   }
-
-   componentWillReceiveProps() {
-      // когда пришол prop
-      console.log("componentWillReceiveProps");
-   }
-
-   shouldComponentUpdate() {
-      // обновлятли component?
-      console.log("shouldComponentUpdate");
-      return true; // обновлять, On reRender
-      // return false; // не обновлять, Off reRender
-   }
-
-   componentWillUpdate() {
-      // до render, после prop
-      console.log("componentWillUpdate");
-   }
-
-   componentDidUpdate() {
-      // после перерисовки или рисовки DOM
-      console.log("componentDidUpdate");
-   }
-
-   componentWillUnmount() {
-      // после удаления элемента из DOM
-      console.log("componentWillUnmount");
-   }
-
-   render() {
-      console.log("render");
-
-      return (
-         <>
-            {this.state.count}
-            <button
-               onClick={() => this.setState({ count: this.state.count + 1 })}
-            >
-               up + 1
-            </button>
-
-            {this.state.count % 2 == 0 ? <FuncComponent/>: <button>Button</button>}
-         </>
-      );
-   }
+   return (
+      <div>
+         <select
+            onChange={({ target }) =>
+               dispatch({ ...counter, payload: Number(target.value) })
+            }
+         >
+            <option value={1}>1</option>
+            <option value={2}>2</option>
+            <option value={3}>3</option>
+            <option value={4}>4</option>
+            <option value={5}>5</option>
+         </select>
+         <button onClick={() => dispatch({ ...counter, type: "minus" })}>
+            minus
+         </button>
+         {counter.count}
+         <button onClick={() => dispatch({ ...counter, type: "plus" })}>
+            plus
+         </button>
+      </div>
+   );
 }
