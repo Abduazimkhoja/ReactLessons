555430e446470ef6e552e247a0e867deb5bfd163
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-05-27 12:32:36 +0500

    localeStorage - create, read, update, delete

--- src/components/one/index.jsx
+++ src/components/one/index.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function One() {
-  return (
-    <div>One</div>
-  )
+   const [count, setCount] = useState(Number(localStorage.getItem("count")));
+   const plus = () => {
+      localStorage.setItem("count", count + 1);
+      setCount(count + 1);
+   };
+   console.log(localStorage.getItem("count"));
+   return (
+      <div>
+         <h1>Count: {count}</h1>
+         <button onClick={plus}>Plus</button>
+         <button onClick={() => localStorage.removeItem("count")}>delete</button>
+         <button onClick={() => localStorage.clear()}>clear</button>
+      </div>
+   );
 }
