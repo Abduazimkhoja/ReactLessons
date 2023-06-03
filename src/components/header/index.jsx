9be89c2d9dc1d256785913470c6416865d38634f
abduazimxoja <abdusalomov.aa03@gmail.com>
2023-06-03 12:37:53 +0500

    react router dom

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,13 +7,13 @@ export default function Header() {
          <nav>
             <List>
                <li>
-                  <Link to="/home">home</Link>
+                  <Link to="/home" activeStyle={{color: "#f00"}}>home</Link>
                </li>
                <li>
-                  <Link to="/about">about</Link>
+                  <Link to="/about" activeStyle={{color: "#f00"}}>about</Link>
                </li>
                <li>
-                  <Link to="/product">product</Link>
+                  <Link to="/product" activeStyle={{color: "#f00"}}>product</Link>
                </li>
             </List>
          </nav>
--- src/components/header/style.js
+++ src/components/header/style.js
@@ -12,6 +12,12 @@ export const Head = styled.header`
 export const Link = styled(NavLink)`
    color: #fff;
    text-decoration: none;
+   text-transform: uppercase;
+
+   transition: color .3s ease;
+   &:hover {
+      color: #f00;
+   }
 `
 export const List = styled.ul`
    height: 44px;
